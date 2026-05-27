8ae778c38104b83ce49867dd12fc613f32f6916c
mudaafi <mudaafi@gmail.com>
2026-05-26 23:46:27 -0400

    update sass to v3, vite to v8, add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -15,8 +16,7 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `
-          @import "./src/assets/colors.scss";
-          @import "./src/assets/sizes.scss";
+          @use "@/assets/main.scss" as *;
         `
       }
     }
